cbc8751b4bd8ba8fb052bb9cd64d002355fdbe9f
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-08-06 16:20:06 -0300

    The first version of my card

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,85 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.cyan,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+                  backgroundImage: NetworkImage('https://github.com/igortauh.png'),
+                  radius: 50.0,
+                ),
+                Text(
+                  'Igor Tauhan',
+                  style: TextStyle(
+                    color: Colors.cyan.shade50,
+                    fontSize: 40.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Container(
+                    color: Colors.cyanAccent.shade200,
+                    height: 125.0,
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Padding(
+                      padding: EdgeInsets.all(15.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            'INFORMATION SYSTEM STUDENT',
+                            style: TextStyle(
+                              color: Colors.cyan.shade50,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          Container(
+                            width: 50,
+                            child: VerticalDivider(
+                              color: Colors.cyan.shade50,
+                              width: 10,
+                            )
+                          ),
+                          Text(
+                            'BACKEND STUDENT',
+                            style: TextStyle(
+                              color: Colors.cyan.shade50,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                          Container(
+                            width: 50,
+                            child: VerticalDivider(
+                              color: Colors.cyan.shade50,
+                              width: 10,
+                            )
+                          ),
+                          Text(
+                            'GAMING AND COMPUTING ENTHUSIAST',
+                            style: TextStyle(
+                              color: Colors.cyan.shade50,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
